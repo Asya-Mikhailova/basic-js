579a7da055d72d804734b519a804d75a73cce8ee
Asya-Mikhailova <54442754+Asya-Mikhailova@users.noreply.github.com>
2020-02-24 15:46:11 +0100

    feat: solve count-cats and carbon-dating tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,20 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string' || sampleActivity == "" || Number(sampleActivity) <=  0 || Number(sampleActivity) > MODERN_ACTIVITY || Number.isNaN(parseFloat(sampleActivity))){
+    return false;
+  } else {
+    let k = 0.693/HALF_LIFE_PERIOD;
+    let t = Math.log((MODERN_ACTIVITY / parseFloat(sampleActivity) ))/k;
+
+    return Math.ceil(t);
+
+  }
+  
+  
+
+ 
+
+  
 };
