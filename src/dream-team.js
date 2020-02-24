3e55e195ca8f24477b18242be794c28389b0d84c
Asya-Mikhailova <54442754+Asya-Mikhailova@users.noreply.github.com>
2020-02-24 16:27:59 +0100

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members)){
+    
+  let team =[];
+  let membersArr=[];
+
+  for(let member of members){
+    if(typeof member == 'string'){
+     membersArr = member.trim().split('');
+      team.push(membersArr[0].toUpperCase());
+    }
+  }
+  return team.sort().join('');
+  } else {
+    return false;
+  }
+  
+
 };
